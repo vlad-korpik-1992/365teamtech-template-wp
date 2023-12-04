94e921dd6696deeb1e458e9f2027a5312693f778
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2023-12-04 16:47:49 +0300

    init template

---
+++ footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+	<div class="wrapper">
+		<div class="footer__box">
+			<nav class="footer__menu">
+				<ul class="footer__menu__list">
+					<li class="footer__menu__list__items">
+						<a href="#" class="footer__menu__link">Home</a>
+					</li>
+					<li class="footer__menu__list__items">
+						<a href="#" class="footer__menu__link">Our services</a>
+					</li>
+					<li class="footer__menu__list__items">
+						<a href="#" class="footer__menu__link">FAQ</a>
+					</li>
+					<li class="footer__menu__list__items">
+						<a href="#" class="footer__menu__link">About</a>
+					</li>
+					<li class="footer__menu__list__items">
+						<a href="#" class="footer__menu__link">Contact</a>
+					</li>
+					<li class="footer__menu__list__items">
+						<a href="#" class="footer__menu__link">Blog</a>
+					</li>
+				</ul>
+			</nav>
+			<div class="footer__copy">
+				<p>365teamtech &copy; <script>
+						var mdate = new Date();
+						document.write(mdate.getFullYear());
+					</script>
+				</p>
+			</div>
+		</div>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php wp_head(); ?>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php wp_title("", true); ?></title>
+</head>
+
+<body>
+	<header class="header">
+		<div class="wrapper">
+			<div class="header__box">
+				<a class="header__link" href="/">
+					<img class="header__logo" src="<?php bloginfo('template_url'); ?>/assets/img/logo.svg" alt="Home">
+				</a>
+				<nav class="menu">
+					<a href="" onclick="openMenu(event)" class="menu__btn">
+						<span class="menu__btn__element"></span>
+					</a>
+					<ul class="menu__list">
+						<li class="menu__list__items">
+							<a class="menu__list__link" href="#">Home</a>
+						</li>
+						<li class="menu__list__items">
+							<a class="menu__list__link" href="#">Our services</a>
+						</li>
+						<li class="menu__list__items">
+							<a class="menu__list__link" href="#" onclick="openDropMenu(event)">FAQ</a>
+							<div class="drop__menu">
+								<a href="#" class="menu__list__link menu__list__link--minw">Smart Switches FAQ</a>
+								<a href="#" class="menu__list__link menu__list__link--minw">Smart Shades / Motorized Blinds FAQ</a>
+							</div>
+						</li>
+						<li class="menu__list__items">
+							<a class="menu__list__link" href="#">About</a>
+						</li>
+						<li class="menu__list__items">
+							<a class="menu__list__link" href="#">Contact</a>
+						</li>
+						<li class="menu__list__items">
+							<a class="menu__list__link" href="#">Blog</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
